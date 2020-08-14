ac6b9effd771da56ff2cd87a07d6e8f772651f9f
ikale1234 <55333848+ikale1234@users.noreply.github.com>
2020-08-14 11:02:32 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Ishaan Kale.");
+		System.out.println("I am a junior at Milpitas High School.");
+		System.out.println("I like to play video games.");
+		System.out.println("My favorite games are League of Legends and Valorant.");
+		System.out.println("I would like to major in Computer Science or Computer Engineering.");
+	}
+}
